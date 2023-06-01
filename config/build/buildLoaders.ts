a4baa7e438e85adbad882aa8f7e0b7555a0c745a
equalsgood <connection.babak@gmail.com>
2023-06-01 23:12:27 +0300

    css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,8 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,7 +11,21 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => resPath.includes('.module.'),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                        exportLocalsConvention: 'camelCase',
+                    },
+                },
+            }
+        ],
     };
 
     return [
