186ea335eeec873a6ca45f3bcabec866e6b73460
equalsgood <connection.babak@gmail.com>
2023-06-06 21:03:05 +0300

    error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Link } from 'react-router-dom';
 import './styles/index.css';
-import { AppRouter } from 'app/providers/router';
+import { AppRouter } from 'app/providers/AppRouter';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div className="app light">
-                <Link to="/">Home</Link>
-                <Link to="bpos">bpo</Link>
-                <AppRouter/>
-            </div>
+            <ErrorBoundary>
+                <div className="app light">
+                    <Link to="/">Home</Link>
+                    <Link to="bpos">bpo</Link>
+                    <AppRouter/>
+                </div>
+            </ErrorBoundary>
         </BrowserRouter>
     );
 };
---
+++ src/app/providers/ErrorBoundary/components/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true, error };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        const { hasError } = this.state;
+
+        if (hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
