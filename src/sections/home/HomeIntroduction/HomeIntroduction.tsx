fa6165ef0930bab21289caa151459a914e615834
equalsgood <connection.babak@gmail.com>
2023-06-13 15:45:38 +0300

    home background

--- src/sections/home/HomeIntroduction/HomeIntroduction.tsx
+++ src/sections/home/HomeIntroduction/HomeIntroduction.tsx
@@ -1,12 +1,16 @@
 import cls from './HomeIntroduction.module.css';
 import { HomeIntroTitle } from './components/HomeIntroTitle/HomeIntroTitle';
 import { HomeSoundExamples } from './components/HomeSoundExamples/HomeSoundExamples';
+import firstBackground from 'shared/assets/images/home-bg-rect-1.png';
+import secondBackground from 'shared/assets/images/home-bg-rect-2.png';
 
 export const HomeIntroduction = () => {
     return (
         <section className={cls.introductionSection}>
             <HomeIntroTitle/>
             <HomeSoundExamples/>
+            <img alt="background image 1" src={firstBackground} className={cls.right} />
+            <img alt="background image 2" src={secondBackground} className={cls.left} />
         </section>
     );
 };
