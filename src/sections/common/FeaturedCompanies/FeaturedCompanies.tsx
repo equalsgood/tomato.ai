1b66919d2bf69033e794fb240cf2a89c09bdfaef
equalsgood <connection.babak@gmail.com>
2023-07-07 01:26:43 +0300

    bpo and enterprise pages tablet adaptations

--- src/sections/common/FeaturedCompanies/FeaturedCompanies.tsx
+++ src/sections/common/FeaturedCompanies/FeaturedCompanies.tsx
@@ -4,11 +4,11 @@ import PointLogo from 'shared/assets/logos/featured/point.png';
 import TribeLogo from 'shared/assets/logos/featured/tribe.png';
 import CardumenLogo from 'shared/assets/logos/featured/cardumen.png';
 import RecursiveLogo from 'shared/assets/logos/featured/recursive.png';
-import OctaveLightLogo from 'shared/assets/logos/featured/octave-light.svg';
-import PointLightLogo from 'shared/assets/logos/featured/point-light.svg';
-import TribeLightLogo from 'shared/assets/logos/featured/tribe-light.svg';
-import CardumenLightLogo from 'shared/assets/logos/featured/cardumen-light.svg';
-import RecursiveLightLogo from 'shared/assets/logos/featured/recursive-light.svg';
+import OctaveLightLogo from 'shared/assets/logos/featured/octave-light.png';
+import PointLightLogo from 'shared/assets/logos/featured/point-light.png';
+import TribeLightLogo from 'shared/assets/logos/featured/tribe-light.png';
+import CardumenLightLogo from 'shared/assets/logos/featured/cardumen-light.png';
+import RecursiveLightLogo from 'shared/assets/logos/featured/recursive-light.png';
 import OctaveTitledLogo from 'shared/assets/logos/featured/octave-titled.svg';
 import PointTitledLogo from 'shared/assets/logos/featured/point-titled.svg';
 import TribeTitledLogo from 'shared/assets/logos/featured/tribe-titled.svg';
@@ -42,11 +42,11 @@ export const FeaturedCompanies = ({ variant, title }: FeaturedCompaniesProps) =>
             }
             { variant === FeaturedCompaniesVariants.LIGHT &&
                 <>
-                    <OctaveLightLogo/>
-                    <PointLightLogo/>
-                    <TribeLightLogo/>
-                    <CardumenLightLogo/>
-                    <RecursiveLightLogo/>
+                    <img alt="logo of the company Octave" className={cls.image} src={OctaveLightLogo} />
+                    <img alt="logo of the company Point" className={cls.image} src={PointLightLogo} />
+                    <img alt="logo of the company Point" className={cls.image} src={TribeLightLogo} />
+                    <img alt="logo of the company Cardumen" className={cls.image} src={CardumenLightLogo} />
+                    <img alt="logo of the company Cardumen" className={cls.image} src={RecursiveLightLogo} />
                 </>
             }
             { variant === FeaturedCompaniesVariants.TITLED &&
