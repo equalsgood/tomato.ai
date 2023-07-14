f7f4eaaaa70d692a5fb385d4398e95eaa090880f
equalsgood <connection.babak@gmail.com>
2023-07-14 19:12:42 +0300

    careers mobile page

--- src/sections/careers/CareersAvailableRoles/components/AvailableRole.tsx
+++ src/sections/careers/CareersAvailableRoles/components/AvailableRole.tsx
@@ -3,9 +3,12 @@ import { CareerRole } from '../CareersAvailableRoles';
 import { Divider, Text, TextVariants } from 'shared/components';
 import LocationIcon from 'shared/assets/icons/location-icon-blue.svg';
 import ArrowIcon from 'shared/assets/icons/arrow-green-right.svg';
+import { useContext } from 'react';
+import { Context } from 'app/providers/ContextProvider';
 
 export const AvailableRole = (props: CareerRole) => {
     const { icon, rate, location, description, title, applyLink } = props;
+    const { isMobile } = useContext(Context);
 
     return (
         <li className={cls.role}>
@@ -14,7 +17,7 @@ export const AvailableRole = (props: CareerRole) => {
             </div>
             <div className={cls.generalInfoContainer}>
                 <div className={cls.titleContainer}>
-                    <Text tag="h3" variant={TextVariants.SUBTITLE_SMALL}>{title}</Text>
+                    <Text classNamesProps={cls.title} tag="h3" variant={TextVariants.SUBTITLE_SMALL}>{title}</Text>
                     <div className={cls.locationContainer}>
                         <div className={cls.locationIcon}>
                             <LocationIcon/>
@@ -24,7 +27,7 @@ export const AvailableRole = (props: CareerRole) => {
                 </div>
                 <Text tag="p" variant={TextVariants.DEFAULT} classNamesProps={cls.description}>{description}</Text>
             </div>
-            <Divider horizontal/>
+            <Divider horizontal={!isMobile}/>
             <div className={cls.rateInfoContainer}>
                 <div className={cls.rateContainer}>
                     <Text tag="p" variant={TextVariants.PARAGRAPH_TITLE} classNamesProps={cls.rateBadge}>Yearly Rate:</Text>
